63a88e484bd7ed9ab5c89fb18d6d249f7b50d944
samuelssnider <sam_redins@yahoo.com>
2017-08-23 11:59:13 -0600

    not sure

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 require './app/models/trip'
->>>>>>> bf8e5b8c9940e895b0ca865663908cacc4231771
 class Condition < ActiveRecord::Base
 	validates :weather_date,     presence: true
 	validates :max_temperature,  presence: true
@@ -14,27 +10,25 @@ class Condition < ActiveRecord::Base
 	validates :precipitation,    presence: true
 	validates :zip_code,         presence: true
 
-<<<<<<< HEAD
 
 	has_many :trips
-=======
 	has_many :trips, class_name: "Trip", foreign_key: "condition_id"
-	
+
 	def self.id_by_date(date)
 		find_by(weather_date: date).id
 	end
-	
+
 	# def self.join_table
 	# 	self.joins(:trips)
 	# end
-		
+
 	def self.breakout_temp(temp_range)
 		range = trip_arr(temp_range)
 		min   = range.values.last
 		max   = range.values.first
 		avg   = range.values.sum / range.values.count
 	end
-	
+
 	def self.trip_arr(temp_range)
 		select("condition.*, sum(trip.condition_id) AS total_trips")
 			.joins(:trips).where(max_temperature: (temp_range...(temp_range + 10)))
@@ -42,7 +36,7 @@ class Condition < ActiveRecord::Base
 		# Condition.joins(:trips).where(max_temperature: (temp_range...(temp_range + 10)))
 		# 	.group(:conditions).order("count_id DESC").count(:id)
 	end
-	
+
 	def self.breakout_temps
 		counter = 50.0
 		until counter == 100
@@ -51,7 +45,6 @@ class Condition < ActiveRecord::Base
 			require "pry"; binding.pry
 		end
 	end
->>>>>>> bf8e5b8c9940e895b0ca865663908cacc4231771
 
 end
 
