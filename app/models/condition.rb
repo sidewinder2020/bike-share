9baef561bd9fcf9889b946610429a0629ac1e2c8
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-23 17:23:18 -0600

    preparing to pull from master in order to fix test errors and work on final tests

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -20,11 +20,6 @@ class Condition < ActiveRecord::Base
 		find_by(weather_date: date).id
 	end
 
-	# def self.join_table
-	# 	self.joins(:trips)
-	# end
-
-		
 	def self.breakout(temp_range)
 		range = trip_temps(temp_range)
 		answers = Hash.new(0)
@@ -33,7 +28,7 @@ class Condition < ActiveRecord::Base
 		answers[:avg] = range.values.sum / range.values.count
 		answers
 	end
-	
+
 	def self.trip_temps(temp_range)
 		select("condition.*, sum(trip.condition_id) AS total_trips")
 			.joins(:trips).where(max_temperature: (temp_range...(temp_range + 10)))
@@ -48,7 +43,7 @@ class Condition < ActiveRecord::Base
 			counter += 10
 		end
 	end
-	
+
 	def self.breakout_inches(precip_range)
 		range = precip_trips(precip_range)
 		answers = Hash.new(0)
@@ -57,7 +52,7 @@ class Condition < ActiveRecord::Base
 		answers[:avg] = range.values.sum / range.values.count
 		answers
 	end
-	
+
 	def self.breakout_precip
 		counter = 0.0
 		temp = Hash.new(0)
@@ -66,13 +61,13 @@ class Condition < ActiveRecord::Base
 			counter += 0.5
 		end
 	end
-	
+
 	def self.precip_trips(range)
 		select("condition.*, sum(trip.condition_id) AS total_trips")
 			.joins(:trips).where(precipitation: (range...(range + 0.5)))
 			.group(:conditions).order("count_id DESC").count(:id)
 	end
-	
+
 	def self.breakout_speed
 		counter = 0.0
 		speed = Hash.new
@@ -81,13 +76,13 @@ class Condition < ActiveRecord::Base
 			count += 4.0
 		end
 	end
-	
+
 	def self.wind_speed_trips(range)
 		select("condition.*, sum(trip.condition_id) AS total_trips")
 			.joins(:trips).where(mean_wind_speed: (range...(range + 4.0)))
 			.group(:conditions).order("count_id DESC").count(:id)
 	end
-	
+
 	def self.breakout_mph(speed_range)
 		range = wind_speed_trips(speed_range)
 		answers = Hash.new(0)
@@ -96,7 +91,7 @@ class Condition < ActiveRecord::Base
 		answers[:avg] = range.values.sum / range.values.count
 		answers
 	end
-	
+
 	def self.breakout_view
 		counter = 0.0
 		speed = Hash.new
@@ -105,13 +100,13 @@ class Condition < ActiveRecord::Base
 			count += 4.0
 		end
 	end
-	
+
 	def self.sight_dist_trips(range)
 		select("condition.*, sum(trip.condition_id) AS total_trips")
 			.joins(:trips).where(mean_visibility: (range...(range + 4.0)))
 			.group(:conditions).order("count_id DESC").count(:id)
 	end
-	
+
 	def self.breakout_sight(speed_range)
 		range = sight_dist_trips(speed_range)
 		answers = Hash.new(0)
@@ -120,12 +115,12 @@ class Condition < ActiveRecord::Base
 		answers[:avg] = range.values.sum / range.values.count
 		answers
 	end
-	
+
 	def self.best_weather_trip_day
 		find( Trip.joins(:condition).group(:condition_id)
 			.order("count_id DESC").count(:id).keys.first).weather_date
 	end
-	
+
 	def self.worst_weather_trip_day
 		find( Trip.joins(:condition).group(:condition_id)
 			.order("count_id DESC").count(:id).keys.last).weather_date
