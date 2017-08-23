76e9ffb3e799233346658d9552c50eb1c554afca
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-08-23 17:53:17 -0600

    fixed trip feature errors

--- spec/features/TRIP/user_can_delete_a_new_trip_spec.rb
+++ spec/features/TRIP/user_can_delete_a_new_trip_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "User can delete a trip" do
     Station.create(name: "Turing", city: "Hell", installation_date: "1969/4/20", dock_count: 69)
 
     visit "/trips"
-    click_on("delete")
+    click_on("Delete")
 
     expect(Trip.count).to eq(0)
   end
